58a6579fd9822de426972f944a37ebee00315511
zakcancode <90767066+zakcancode@users.noreply.github.com>
2021-09-15 12:36:19 +0100

    Update main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,7 +1,7 @@
 input.onButtonPressed(Button.A, function () {
-    basic.showIcon(IconNames.No)
     music.stopAllSounds()
     music.setBuiltInSpeakerEnabled(false)
+    basic.showIcon(IconNames.No)
 })
 input.onSound(DetectedSound.Loud, function () {
     music.setBuiltInSpeakerEnabled(true)
@@ -32,8 +32,8 @@ input.onSound(DetectedSound.Loud, function () {
 })
 input.onButtonPressed(Button.B, function () {
     music.setBuiltInSpeakerEnabled(true)
-    basic.showIcon(IconNames.Duck)
     while (true) {
+        basic.showIcon(IconNames.Duck)
         for (let index = 0; index < 2; index++) {
             music.playTone(220, music.beat(BeatFraction.Quarter))
             music.rest(music.beat(BeatFraction.Quarter))
@@ -58,31 +58,8 @@ input.onButtonPressed(Button.B, function () {
     }
 })
 input.onSound(DetectedSound.Quiet, function () {
-    music.setBuiltInSpeakerEnabled(true)
-    basic.showIcon(IconNames.Duck)
-    while (true) {
-        for (let index = 0; index < 2; index++) {
-            music.playTone(220, music.beat(BeatFraction.Quarter))
-            music.rest(music.beat(BeatFraction.Quarter))
-            music.playTone(220, music.beat(BeatFraction.Quarter))
-            music.rest(music.beat(BeatFraction.Quarter))
-            music.playTone(262, music.beat(BeatFraction.Quarter))
-            music.rest(music.beat(BeatFraction.Whole))
-        }
-        music.playTone(220, music.beat(BeatFraction.Quarter))
-        music.rest(music.beat(BeatFraction.Quarter))
-        music.playTone(262, music.beat(BeatFraction.Quarter))
-        music.rest(music.beat(BeatFraction.Quarter))
-        music.playTone(349, music.beat(BeatFraction.Half))
-        music.rest(music.beat(BeatFraction.Half))
-        music.playTone(330, music.beat(BeatFraction.Half))
-        music.rest(music.beat(BeatFraction.Half))
-        music.playTone(294, music.beat(BeatFraction.Quarter))
-        music.rest(music.beat(BeatFraction.Eighth))
-        music.playTone(294, music.beat(BeatFraction.Quarter))
-        music.rest(music.beat(BeatFraction.Half))
-        music.playTone(262, music.beat(BeatFraction.Whole))
-    }
+    music.stopAllSounds()
+    music.setBuiltInSpeakerEnabled(false)
 })
 basic.showIcon(IconNames.SmallDiamond)
 basic.clearScreen()
